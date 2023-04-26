f51d2ef19a33367ca528ce18fa1771926b8ac94d
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-04-26 19:37:19 +1000

    name mixins better

--- src/main/java/folk/sisby/totem_anywhere_standalone/mixin/LivingEntityMixin.java
+++ src/main/java/folk/sisby/totem_anywhere_standalone/mixin/LivingEntityMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
-public class CheckInventoryMixin {
+public class LivingEntityMixin {
 	@Redirect(method = "tryUseTotem", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getStackInHand(Lnet/minecraft/util/Hand;)Lnet/minecraft/item/ItemStack;"))
 	private ItemStack replaceHandStackWithInventoryStack(LivingEntity livingEntity, Hand hand) {
 		return (livingEntity instanceof PlayerEntity player && player.getInventory().contains(Items.TOTEM_OF_UNDYING.getDefaultStack()) && player.getOffHandStack().getItem() != Items.TOTEM_OF_UNDYING && player.getMainHandStack().getItem() != Items.TOTEM_OF_UNDYING) ? player.getInventory().getStack(player.getInventory().getSlotWithStack(Items.TOTEM_OF_UNDYING.getDefaultStack())) : livingEntity.getStackInHand(hand);
