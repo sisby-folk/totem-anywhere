7dc2de49a07397a53b0c6e02401b445883a56974
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-06-26 15:49:44 +1000

    use modifyVariable instead of redirect

--- src/main/java/folk/sisby/totem_anywhere/mixin/LivingEntityMixin.java
+++ src/main/java/folk/sisby/totem_anywhere/mixin/LivingEntityMixin.java
@@ -6,13 +6,19 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+@SuppressWarnings("ConstantConditions")
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
-	@Redirect(method = "tryUseTotem", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getStackInHand(Lnet/minecraft/util/Hand;)Lnet/minecraft/item/ItemStack;"))
-	private ItemStack replaceHandStackWithInventoryStack(LivingEntity livingEntity, Hand hand) {
-		return (livingEntity instanceof PlayerEntity player && player.getInventory().contains(Items.TOTEM_OF_UNDYING.getDefaultStack()) && player.getOffHandStack().getItem() != Items.TOTEM_OF_UNDYING && player.getMainHandStack().getItem() != Items.TOTEM_OF_UNDYING) ? player.getInventory().getStack(player.getInventory().getSlotWithStack(Items.TOTEM_OF_UNDYING.getDefaultStack())) : livingEntity.getStackInHand(hand);
+public abstract class LivingEntityMixin {
+	@ModifyVariable(method = "tryUseTotem", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Hand;values()[Lnet/minecraft/util/Hand;", ordinal = 0), ordinal = 0)
+	private ItemStack totemStackInventoryFallback(ItemStack totemStack) {
+		if ((Object) this instanceof PlayerEntity player && player.getInventory().contains(Items.TOTEM_OF_UNDYING.getDefaultStack())) {
+			return player.getInventory().getStack(player.getInventory().getSlotWithStack(Items.TOTEM_OF_UNDYING.getDefaultStack()));
+		}
+		return totemStack;
 	}
 }
